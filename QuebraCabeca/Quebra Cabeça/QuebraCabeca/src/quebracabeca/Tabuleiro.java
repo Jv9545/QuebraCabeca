bb1293a6e6de7c7a7e8ee1f4c7b652ce6c67ec0b
Jv9545 <160799692+Jv9545@users.noreply.github.com>
2024-12-04 22:10:33 -0300

    Add files via upload

--- QuebraCabeca/Quebra Cabeça/QuebraCabeca/src/quebracabeca/Tabuleiro.java
+++ QuebraCabeca/Quebra Cabeça/QuebraCabeca/src/quebracabeca/Tabuleiro.java
@@ -19,7 +19,7 @@ public class Tabuleiro {
     public boolean adicionarPeca(Peca peca, int linha, int coluna) {
         //verifica se a posição é vazia
         if (grid[linha][coluna] == null) {
-            //verifica se é a primeira posição da matriz
+            //verifica se é a posição superior esquerda da matriz
             if (linha == 0 && coluna == 0) {
                 //verifica se a borda superior e esquerda são preenchidas
                 if (peca.getBorda(0) == 3 && peca.getBorda(3) == 3 ) {
@@ -34,12 +34,51 @@ public class Tabuleiro {
                 }else{
                     System.out.println("A peça nao se encaixa com o tabuleiro");
                 }
-            }else if(linha == 0 && coluna == grid[0].length - 1){
-                
-            }else if(linha == grid.length - 1 && coluna == 0){
-                
-            }else if(linha == grid.length - 1 && coluna == grid[0].length - 1){
-                
+            }
+            //verifica se é a posição superior direita da matriz
+            else if(linha == 0 && coluna == grid[0].length - 1){
+                if (peca.getBorda(0) == 3 && peca.getBorda(1) == 3 ) {
+                    //verifica se a borda da peça inserida é encaixavel com a de traz e a de baixo dela
+                    if(peca.getBorda(3) != grid[linha][coluna-1].getBorda(1) && peca.getBorda(2) != grid[linha+1][coluna].getBorda(0) ){
+                        //inserção
+                        grid[linha][coluna] = peca;
+                        System.out.println("A peça foi encaixada");
+                    }else{
+                        System.out.println("A peça nao se encaixa com a da esquerda ou com a de baixo.");
+                    }
+                }else{
+                    System.out.println("A peça nao se encaixa com o tabuleiro");
+                }
+            }
+            //verifica se é a posição inferior esquerda da matriz
+            else if(linha == grid.length - 1 && coluna == 0){
+                if (peca.getBorda(2) == 3 && peca.getBorda(3) == 3 ) {
+                    //verifica se a borda da peça inserida é encaixavel com a de cima e a da frente dela
+                    if(peca.getBorda(0) != grid[linha-1][coluna].getBorda(2) && peca.getBorda(1) != grid[linha][coluna+1].getBorda(3) ){
+                        //inserção
+                        grid[linha][coluna] = peca;
+                        System.out.println("A peça foi encaixada");
+                    }else{
+                        System.out.println("A peça nao se encaixa com a da frente ou com a de baixo.");
+                    }
+                }else{
+                    System.out.println("A peça nao se encaixa com o tabuleiro");
+                }
+            }
+            //verifica se é a posição inferior direita da matriz
+            else if(linha == grid.length - 1 && coluna == grid[0].length - 1){
+                if (peca.getBorda(1) == 3 && peca.getBorda(2) == 3 ) {
+                    //verifica se a borda da peça inserida é encaixavel com a de cima e a da traz dela
+                    if(peca.getBorda(0) != grid[linha-1][coluna].getBorda(2) && peca.getBorda(3) != grid[linha][coluna+1].getBorda(0) ){
+                        //inserção
+                        grid[linha][coluna] = peca;
+                        System.out.println("A peça foi encaixada");
+                    }else{
+                        System.out.println("A peça nao se encaixa com a da frente ou com a de baixo.");
+                    }
+                }else{
+                    System.out.println("A peça nao se encaixa com o tabuleiro");
+                }
             }
 
             return true;
